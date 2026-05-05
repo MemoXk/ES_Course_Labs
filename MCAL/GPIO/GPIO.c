fbbbbcd3ee2ecd0db3a56e1a00e873f1c538f279
memki <memki@MEMO>
2026-05-05 18:31:18 +0300

    fix: stabilize ultrasonic telemetry GPIO timing

--- MCAL/GPIO/GPIO.c
+++ MCAL/GPIO/GPIO.c
@@ -4,6 +4,11 @@
 
 #include "../../SERVICES/BIT_MATH.h"
 
+static u8 gpio_porta_latch = GPIO_PORTA_INIT_VAL;
+static u8 gpio_portb_latch = GPIO_PORTB_INIT_VAL;
+static u8 gpio_portc_latch = GPIO_PORTC_INIT_VAL;
+static u8 gpio_portd_latch = GPIO_PORTD_INIT_VAL;
+static u8 gpio_porte_latch = GPIO_PORTE_INIT_VAL;
 
 void GPIO_SetPinDirection(u8 Port, u8 Pin, u8 Direction)
 {
@@ -56,37 +61,42 @@ void GPIO_SetPinValue(u8 Port, u8 Pin, u8 Value)
     {
         case GPIO_PORTA:
             if(Value == GPIO_HIGH)
-                SET_BIT(PORTA, Pin);
+                SET_BIT(gpio_porta_latch, Pin);
             else
-                CLR_BIT(PORTA, Pin);
+                CLR_BIT(gpio_porta_latch, Pin);
+            PORTA = gpio_porta_latch;
         break;
 
         case GPIO_PORTB:
             if(Value == GPIO_HIGH)
-                SET_BIT(PORTB, Pin);
+                SET_BIT(gpio_portb_latch, Pin);
             else
-                CLR_BIT(PORTB, Pin);
+                CLR_BIT(gpio_portb_latch, Pin);
+            PORTB = gpio_portb_latch;
         break;
 
         case GPIO_PORTC:
             if(Value == GPIO_HIGH)
-                SET_BIT(PORTC, Pin);
+                SET_BIT(gpio_portc_latch, Pin);
             else
-                CLR_BIT(PORTC, Pin);
+                CLR_BIT(gpio_portc_latch, Pin);
+            PORTC = gpio_portc_latch;
         break;
 
         case GPIO_PORTD:
             if(Value == GPIO_HIGH)
-                SET_BIT(PORTD, Pin);
+                SET_BIT(gpio_portd_latch, Pin);
             else
-                CLR_BIT(PORTD, Pin);
+                CLR_BIT(gpio_portd_latch, Pin);
+            PORTD = gpio_portd_latch;
         break;
 
         case GPIO_PORTE:
             if(Value == GPIO_HIGH)
-                SET_BIT(PORTE, Pin);
+                SET_BIT(gpio_porte_latch, Pin);
             else
-                CLR_BIT(PORTE, Pin);
+                CLR_BIT(gpio_porte_latch, Pin);
+            PORTE = gpio_porte_latch;
         break;
 
         default:
@@ -130,15 +140,21 @@ u8 GPIO_GetPinValue(u8 Port, u8 Pin)
 
 void GPIO_Init(void)
 {
+    gpio_porta_latch = GPIO_PORTA_INIT_VAL;
+    gpio_portb_latch = GPIO_PORTB_INIT_VAL;
+    gpio_portc_latch = GPIO_PORTC_INIT_VAL;
+    gpio_portd_latch = GPIO_PORTD_INIT_VAL;
+    gpio_porte_latch = GPIO_PORTE_INIT_VAL;
+
     TRISA = GPIO_PORTA_DIR;
     TRISB = GPIO_PORTB_DIR;
     TRISC = GPIO_PORTC_DIR;
     TRISD = GPIO_PORTD_DIR;
     TRISE = GPIO_PORTE_DIR;
 
-    PORTA = GPIO_PORTA_INIT_VAL;
-    PORTB = GPIO_PORTB_INIT_VAL;
-    PORTC = GPIO_PORTC_INIT_VAL;
-    PORTD = GPIO_PORTD_INIT_VAL;
-    PORTE = GPIO_PORTE_INIT_VAL;
+    PORTA = gpio_porta_latch;
+    PORTB = gpio_portb_latch;
+    PORTC = gpio_portc_latch;
+    PORTD = gpio_portd_latch;
+    PORTE = gpio_porte_latch;
 }
